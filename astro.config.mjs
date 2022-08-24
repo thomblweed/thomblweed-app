0472d1c7bfda17531d2c34b93b83bf2baab04477
Thomas Newman <thomblweed@gmail.com>
2022-08-24 11:14:37 +0100

    vercel adapter install and setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
-  server: { port: 2000 }
+  server: { port: 2000 },
+  adapter: vercel(),
+  output: 'server'
 });
